3d957844580a5ed9e18ee09d5b1615ff70d4d834
kei <kei.takakuda.0415@icloud.com>
2022-02-14 05:49:22 +0900

    feat: Use functional middleware

--- hello-nest/src/app.module.ts
+++ hello-nest/src/app.module.ts
@@ -1,8 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { logger } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
@@ -12,7 +13,7 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
       consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: 'cats', method: RequestMethod.GET});
+      .apply(logger)
+      .forRoutes(CatsController);
   }
 }
