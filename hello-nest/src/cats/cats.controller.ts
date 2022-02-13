1c4ef396f3d8ae7de88a9705274bf79ef35988fd
kei <kei.takakuda.0415@icloud.com>
2022-02-14 03:39:27 +0900

    feat: Change controller implementation

--- hello-nest/src/cats/cats.controller.ts
+++ hello-nest/src/cats/cats.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { CreateCatDto } from 'src/dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService) {}
+
+
     @Post()
     async create(@Body() createCatDto: CreateCatDto) {
-        return 'This action adds a new cat';
+        this.catsService.create(createCatDto)
     }
 
     @Get()
-    async findAll(): Promise<any[]> {
-        return [];
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `This action returns a #${id} cat`;
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
     }
 }
