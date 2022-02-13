2a78a5f347eee73cd164d10a8769aaf6d5c763e2
kei <kei.takakuda.0415@icloud.com>
2022-02-14 02:42:40 +0900

    feat: Add Post route

--- hello-nest/src/cats/cats.controller.ts
+++ hello-nest/src/cats/cats.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+    @Post()
+    create(): string {
+        return 'This action adds a new cat';
+    }
+
     @Get()
     findAll(@Req() request: Request): string {
         return 'This action return all cats'
